10b243c7570c04c8766aa272d3b6ebd7830ebcd1
Franz Wollang <franzwollang@gmail.com>
2024-04-27 17:58:52 +0200

    simplified providers

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,4 +1,8 @@
+import ProvidersClient from '@/components/providersClient';
+import { SupportedLocale } from '@/i18n';
 import '@/styles/globals.css';
+import pick from 'just-pick';
+import { NextIntlClientProvider, useMessages } from 'next-intl';
 
 import { Inter } from 'next/font/google';
 
@@ -14,13 +18,26 @@ export const metadata = {
 };
 
 export default function RootLayout({
-  children
+  children,
+  params: { locale }
 }: {
   children: React.ReactNode;
+  params: { locale: SupportedLocale };
 }) {
+  const messages = useMessages();
+  
   return (
     <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>{children}</body>
+      <body className={`font-sans ${inter.variable}`}>
+        <NextIntlClientProvider
+          messages={pick(messages, ["UI"])}
+          locale={locale}
+        > 
+          <ProvidersClient>
+            {children}
+          </ProvidersClient>
+        </NextIntlClientProvider>
+      </body>
     </html>
   );
 }
